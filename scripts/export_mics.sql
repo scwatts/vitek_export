029a7958908af2a8315089922a5bbb8d08c7a7f8
scwatts <s.watts2@student.unimelb.edu.au>
2021-04-16 10:18:35 +1000

    Add full sql query to export_mics.sql as comment

--- scripts/export_mics.sql
+++ scripts/export_mics.sql
@@ -1,2 +1,10 @@
+/*
+Using this as a query:
+  SELECT isolate.id, isolate.labid, isolate.isolate_number, isolate.creation_date_timestamp, isolate.final_date_timestamp, ren_drug.mnemo, ren_drug.long_name, assay_ast_analysis_result.mic, assay_ast_analysis_result.relationship_operator
+   FROM isolate
+   INNER JOIN assay ON isolate.id = assay.ast_assay_isolate_id
+   RIGHT JOIN assay_ast_analysis_result ON assay.ast_assay_result_id = assay_ast_analysis_result.ast_assay_result_id
+   INNER JOIN ren_drug ON assay_ast_analysis_result.drug_id = ren_drug.id;
+*/
 \c vitek_db
 \copy (SELECT isolate.id, isolate.labid, isolate.isolate_number, isolate.creation_date_timestamp, isolate.final_date_timestamp, ren_drug.mnemo, ren_drug.long_name, assay_ast_analysis_result.mic, assay_ast_analysis_result.relationship_operator FROM isolate INNER JOIN assay ON isolate.id = assay.ast_assay_isolate_id RIGHT JOIN assay_ast_analysis_result ON assay.ast_assay_result_id = assay_ast_analysis_result.ast_assay_result_id INNER JOIN ren_drug ON assay_ast_analysis_result.drug_id = ren_drug.id) TO STDOUT CSV HEADER
